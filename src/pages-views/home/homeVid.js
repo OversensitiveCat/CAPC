29c2fc960d579777392cc395e1228c3b6a6925fd
OversensitiveCat <124178833+OversensitiveCat@users.noreply.github.com>
2023-09-23 20:10:41 +0200

    home video

--- src/pages-views/home/homeVid.js
+++ src/pages-views/home/homeVid.js
@@ -3,25 +3,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(ScrollTrigger)
 
-import vid from '../../data/presentation.webm'
-
 const homeVid = () => {
-  const url = new URL(vid, import.meta.url).href
-  console.log(url)
-
-  const v = document.querySelector('#home-vid')
-  const s = v.querySelector('source')
-
-  v.onerror = function () {
-    console.log('Error video')
-    gsap.set(v, { display: 'none' })
-  }
-
-  s.addEventListener('error', function () {
-    console.log('Error loading video')
-    gsap.set(v, { display: 'none' })
-  })
-
   // Anim
   let tl = gsap.timeline({ paused: true })
   tl.from('.home-expo-video', {
